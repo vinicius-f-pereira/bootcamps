a24094af7ced37a0adf80ec50161f6cbee353163
nivicius <niviciusdev@gmail.com>
2023-12-25 22:53:30 -0300

    Update adding some comments to know what Im doing

--- c-sharp/Models/Person.cs
+++ c-sharp/Models/Person.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Threading.Tasks;
 
+/* namespace are like env path
+methods can be called as functions too
+ */
 namespace c_sharp.Models
 {
 	public class Person
