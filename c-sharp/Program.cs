06070636ffe80dff2e5a99a78152f14620ba3f56
nivicius <niviciusdev@gmail.com>
2023-12-27 01:22:40 -0300

    fished repetition structures

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -3,104 +3,104 @@ using System.Formats.Asn1;
 using System.Xml.Serialization;
 using c_sharp.Models; // it's like a include in c lang.
 
-// Person person1 = new Person(); // it's like a function that returns a struct initialized.
+Person person1 = new Person(); // it's like a function that returns a struct initialized.
 
-// person1.Name = "Thalia Pereira";
-// person1.Age = 3;
-// person1.Greeting();
+person1.Name = "Thalia Pereira";
+person1.Age = 3;
+person1.Greeting();
 
-// // some types to work with numbers.
-// int amount = 1;
-// double height = 1.80;
-// decimal price = 1.90M; // this is a good variable to work with currency or things that you need more precision in decimals.
+// some types to work with numbers.
+int amount = 1;
+double height = 1.80;
+decimal price = 1.90M; // this is a good variable to work with currency or things that you need more precision in decimals.
 
-// Console.WriteLine("Amount \n" + amount);
-// Console.WriteLine("Height \n" + height.ToString("0.00")); // this is needed to print within full decimals.
-// Console.WriteLine("Price \n" + price);
+Console.WriteLine("Amount \n" + amount);
+Console.WriteLine("Height \n" + height.ToString("0.00")); // this is needed to print within full decimals.
+Console.WriteLine("Price \n" + price);
 
-// // we have a type to work with data and time too.
-// DateTime dataNow = DateTime.Now.AddDays(amount);
-// Console.WriteLine(dataNow);
-// dataNow = DateTime.Now.AddDays(5);
-// Console.WriteLine(dataNow.ToString("dd/MM/yyyy"));
+// we have a type to work with data and time too.
+DateTime dataNow = DateTime.Now.AddDays(amount);
+Console.WriteLine(dataNow);
+dataNow = DateTime.Now.AddDays(5);
+Console.WriteLine(dataNow.ToString("dd/MM/yyyy"));
 
-// /* ******************************** */
+/* ******************************** */
 
-// // Casting
+// Casting
 
-// /* int number = "5" */
-// long number = Convert.ToInt32("5");
-// // number = Convert.ToUInt16("5");
-// // number = Convert.ToInt64("5");
-// Console.WriteLine(number);
-// /**
-// * Some types can't be converted because don't fit in anothers.
-// * like try convert to int64 if the variable was declared as int (same as int32)
-// */
+/* int number = "5" */
+long number = Convert.ToInt32("5");
+// number = Convert.ToUInt16("5");
+// number = Convert.ToInt64("5");
+Console.WriteLine(number);
+/**
+* Some types can't be converted because don't fit in anothers.
+* like try convert to int64 if the variable was declared as int (same as int32)
+*/
 
-// /**
-// * We can use Parse too, but parse can fail if data is invalid. Parse will stop
-// * the program, while convert if receive a null input will
-// * return '0'. */
+/**
+* We can use Parse too, but parse can fail if data is invalid. Parse will stop
+* the program, while convert if receive a null input will
+* return '0'. */
 
-// string test = Convert.ToString(5); /* not common way to convert */
-// test = number.ToString(); // better way to convert a number
+string test = Convert.ToString(5); /* not common way to convert */
+test = number.ToString(); // better way to convert a number
 
-// Console.WriteLine(test);
+Console.WriteLine(test);
 
 
-// /**
-// * implict casting as talked above, when a type fit in another type we can
-// * use a implicit casting. e.g. (int a = 5, double b = a).
-//  */
+/**
+* implict casting as talked above, when a type fit in another type we can
+* use a implicit casting. e.g. (int a = 5, double b = a).
+ */
 
-// /**
-// * the best way to convert in c# (until now)
-// */
+/**
+* the best way to convert in c# (until now)
+*/
 
-// string source = "15-";
-// int x = 0;
+string source = "15-";
+int x = 0;
 
-// int.TryParse(source, out x);
+int.TryParse(source, out x);
 
-// Console.WriteLine(x);
-// Console.WriteLine("Conversion done successfully");
+Console.WriteLine(x);
+Console.WriteLine("Conversion done successfully");
 
-// /* if and else statement are like in pure c lang. */
+/* if and else statement are like in pure c lang. */
 
-// int stock = 10;
-// int salesAmount = 0;
-// bool enough = salesAmount > 0 && stock >= salesAmount;
+int stock = 10;
+int salesAmount = 0;
+bool enough = salesAmount > 0 && stock >= salesAmount;
 
-// Console.WriteLine($"Stock: {stock}");
-// Console.WriteLine($"Customer amount: {salesAmount}");
-// Console.WriteLine($"We have in stock?: {enough}");
+Console.WriteLine($"Stock: {stock}");
+Console.WriteLine($"Customer amount: {salesAmount}");
+Console.WriteLine($"We have in stock?: {enough}");
 
-// if (salesAmount == 0)
-// 	Console.WriteLine("Invalid amount");
-// else if (enough)
-// 	Console.WriteLine("Sold");
-// else
-// 	Console.WriteLine("Sorry, out of stock.");
+if (salesAmount == 0)
+	Console.WriteLine("Invalid amount");
+else if (enough)
+	Console.WriteLine("Sold");
+else
+	Console.WriteLine("Sorry, out of stock.");
 
-// /* switch case */
+/* switch case */
 
-// Console.WriteLine("Type a letter");
-// string letter = Console.ReadLine();
+Console.WriteLine("Type a letter");
+string letter = Console.ReadLine();
 
-// switch (letter)
-// {
-// 	case "a":
-// 	case "e":
-// 	case "i":
-// 	case "o":
-// 	case "u":
-// 		Console.WriteLine("Vowel");
-// 		break;
-// 	default:
-// 		Console.WriteLine("Not Vowel");
-// 		break;
-// }
+switch (letter)
+{
+	case "a":
+	case "e":
+	case "i":
+	case "o":
+	case "u":
+		Console.WriteLine("Vowel");
+		break;
+	default:
+		Console.WriteLine("Not Vowel");
+		break;
+}
 
 Calculator calc = new Calculator();
 
@@ -114,3 +114,60 @@ calc.Sin(90);
 calc.Cos(90);
 calc.Tan(90);
 calc.SquareRoot(9);
+
+int nbr = 10;
+
+for (int count = 1; count <= nbr; count++)
+	Console.WriteLine($"{count} x {nbr} = {nbr * count}");
+
+
+int soma = 0, numero = 0;
+
+do
+{
+	Console.WriteLine("type a number: ");
+	numero = Convert.ToInt32(Console.ReadLine());
+	soma += numero;
+} while (numero != 0);
+Console.WriteLine($"{soma}");
+
+string option;
+bool showMenu = true;
+
+while (showMenu)
+{
+	Console.Clear();
+	Console.WriteLine($"Select an option:");
+	Console.WriteLine($"1 - Register customer");
+	Console.WriteLine($"2 - Search customer:");
+	Console.WriteLine($"3 - Delete customer");
+	Console.WriteLine($"4 - Exit");
+
+	option = Console.ReadLine();
+
+	switch (option)
+	{
+		case "1":
+			Console.WriteLine("Insert Data");
+			break;
+
+		case "2":
+			Console.WriteLine("Find Customer");
+			break;
+
+		case "3":
+			Console.WriteLine("Delete a costumer");
+			break;
+
+		case "4":
+			Console.WriteLine("Exit");
+			showMenu = false;
+			break;
+
+		default:
+			Console.WriteLine("Invalid option");
+			break;
+	}
+}
+
+Console.WriteLine("THE END");
