4a8695d016f1d0d535fa5ed5f3ba04c603514c19
nivicius <niviciusdev@gmail.com>
2023-12-30 02:10:55 -0300

    feat: new module

---
+++ exploring/Models/Person.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exploring.Models
+{
+	public class Person
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+	}
+}
+
+/* this module is to see deeply about get, set, methods */
