304290932ac2869eb3292390e299420efeb80bb4
nivicius <niviciusdev@gmail.com>
2024-01-01 20:34:29 -0300

    feat changed program to use course class code.

--- exploring/Program.cs
+++ exploring/Program.cs
@@ -1,7 +1,23 @@
-﻿using exploring.Models;
+﻿using System.Net.Security;
+using exploring.Models;
 
 Person p1 = new Person();
 p1.Name = "Stete";
 p1.LastName = "Freitas";
-p1.Age = 23;
-p1.Greeting();
+
+Person p2 = new Person();
+p2.Name = "Lila";
+p2.LastName = "Freitas";
+
+Person p3 = new Person();
+p3.Name = "Nivi";
+p3.LastName = "Freitas";
+
+Course englishCourse = new Course();
+englishCourse.Name = "English";
+englishCourse.Students = new List<Person>();
+
+englishCourse.AddStudent(p1);
+englishCourse.AddStudent(p2);
+englishCourse.AddStudent(p3);
+englishCourse.ListStudents();
